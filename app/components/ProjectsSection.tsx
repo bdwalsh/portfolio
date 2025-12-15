14eee79296e2feac9d267ede4198b20d03625b4d
Brendan Walsh <bdwalsh075@gmail.com>
2025-12-14 21:16:51 -0800

    initial project component

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -1,13 +1,13 @@
-import ProjectDetials from "@/app/components/projects/ProjectDetails"
+import ProjectDetails from "@/app/components/projects/ProjectDetails"
 
 const ProjectsSection = () => {
     return (
         <section id="work" className="bg-brand-primary flex flex-col items-center justify-center px-4 py-6 md:px-8 md:py-10 lg:px-[100px]">
             <div className="w-full lg:max-w-[75rem]">
                 <h1 className="text-white text-heading-h1 font-heading-h1-600">Projects I've Worked On</h1>
-                <div className="h-2 w-8 bg-accent mb-5"></div>
+                <div className="h-2 w-8 bg-accent mb-12"></div>
 
-                <ProjectDetials />
+                <ProjectDetails title="Studio Manager - Pixieset" description="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur"/>
             </div>
         </section>
     );
--- app/components/projects/ProjectDetails.tsx
+++ app/components/projects/ProjectDetails.tsx
@@ -1,7 +1,52 @@
-const WorkDetailsProjectOverview = () => {
+import Image from 'next/image'
+
+
+interface ProjectDetailsProps {
+    title: string;
+    description?: string; 
+    id?: number;
+    featureImage?: string;
+    icons?: string[];
+}
+
+const icons = [
+    "/images/icons/vue-js-icon.svg",
+    "/images/icons/laravel-icon.svg",
+    "/images/icons/typescript-icon.svg",
+    "/images/icons/jest-js-icon.svg",
+]
+
+const ProjectDetails = ({ title, description, id }: ProjectDetailsProps) => {
     return (
-        <div>Work Details</div>
+        <div className="grid grid-cols-3 gap-x-8">
+            <Image
+                src="/images/studio-manager-homepage-screen.png"
+                alt="Studio Manager Preview Image"
+                width={1920}
+                height={958}
+                className="col-span-1"
+            />
+            <div className="col-span-2">
+                <h2 className="text-heading-h2 mb-2">{title}</h2>
+                {description && <p>{description}</p>}
+                {id && <p>ID: {id}</p>}
+                <ul className="flex mt-2">
+                    {icons.map((icon, index) => (
+                        // Each child in a list should have a unique "key" prop
+                        <li key={index}>
+                            <Image
+                                src={icon}
+                                alt="Studio Manager Preview Image"
+                                width={25}
+                                height={25}
+                                className="mr-4"
+                            />
+                        </li> 
+                    ))}
+                </ul>
+            </div>
+        </div>
     );
 }
 
-export default WorkDetailsProjectOverview;
+export default ProjectDetails;
